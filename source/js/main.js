47f77fd3fb26fce971e4a4f1cb56c2a110f04721
PoushnayaA <poushnaya@bk.ru>
2024-03-15 21:11:52 +0300

    Cookie

--- source/js/main.js
+++ source/js/main.js
@@ -32,6 +32,70 @@ window.addEventListener('DOMContentLoaded', () => {
     })
   }
 
+  const cookieModal = document.querySelector('.modal-cookie');
+  if (cookieModal) {
+    if (cookieModal.classList.contains('active')) {
+      cookieModal.querySelector('.cookie').classList.remove('visually-hidden');
+      document.querySelector('.wrapper').classList.add('dark');
+    }
+  }
+
+
+  const cookieButtons = document.querySelector('.cookies-form-buttons');
+  if (cookieButtons) {
+    const allowButton = cookieButtons.querySelector('button[type="button"]');
+    if (allowButton) {
+      allowButton.addEventListener('click', function () {
+        document.querySelectorAll('input[name="cookie-types"]').forEach(i => {
+          i.checked = true;
+        });
+      })
+    }
+
+    const saveButtons = document.querySelectorAll('button[type="submit"]');
+    if (saveButtons) {
+      saveButtons.forEach(saveButton => {
+        saveButton.addEventListener('click', function (evt) {
+          evt.preventDefault()
+          document.querySelector('.wrapper').classList.remove('dark');
+          cookieModal.classList.remove('active');
+          cookieModal.querySelector('.cookie').classList.add('visually-hidden');
+          cookieModal.querySelector('.cookie-set').classList.add('visually-hidden');
+        })
+      });
+    }
+  }
+
+
+
   // ---------------------------------
 
 });
+
+
+
+// // Получаем ссылки на элементы меню и контейнера
+// const menu = document.querySelector('.menu');
+// const menuContainer = document.querySelector('.menu-container');
+
+// // Функция для закрытия меню
+// function closeMenu(event) {
+//   // Проверяем, что клик был сделан вне области меню
+//   if (!menu.contains(event.target) && !menuContainer.contains(event.target)) {
+//     // Закрываем меню (например, удаляем класс 'open')
+//     menu.classList.remove('open');
+//     // Удаляем обработчик события с документа
+//     document.removeEventListener('click', closeMenu);
+//   }
+// }
+
+// // Функция для открытия меню
+// function openMenu() {
+//   // Открываем меню (например, добавляем класс 'open')
+//   menu.classList.add('open');
+//   // Добавляем обработчик события клика на документ
+//   document.addEventListener('click', closeMenu);
+// }
+
+// // Вызываем функцию openMenu при необходимости
+// // (например, при клике на кнопку или ссылку)
